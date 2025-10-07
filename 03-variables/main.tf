e5e06ad628bd7e5ac68109f9b191344f6a9310eb
tushar <tusharlevesl@icloud.com>
2025-10-06 22:44:06 -0400

    tf

---
+++ 03-variables/main.tf
@@ -0,0 +1,88 @@
+variable "x" {
+  default =  10
+}
+
+output "x" {
+  value = var.x
+}
+
+
+variable "num" {
+  default =  100
+}
+
+variable "string" {
+  default =  "HELLO"
+}
+
+
+variable "boolean" {
+  default =  true
+}
+
+output "string" {
+  value = "${var.string}, is a string"
+}
+
+
+
+
+
+# normal, LIST, MAP/ DICT
+
+variable "normal" {
+  default =  true
+}
+
+
+variable "list" {
+  default =  [
+    "d",
+    1,
+    3,
+    false
+  ]
+}
+
+
+variable "map" {
+  default = {
+    x    = 10
+    y    = "Why"
+    ooga = true
+  }
+}
+
+output "list" {
+  value = var.list[0]
+}
+
+output "map" {
+  value = var.map["y"]
+}
+
+
+#CLI variable
+
+
+variable "cli" {}
+
+output "command-line-variable" {
+  value = "var.cli"
+}
+
+variable "v" {}
+
+output "var-form-tfvars" {
+  value = var.v
+
+}
+
+
+
+variable "env" {}
+
+output "var-form-devvars" {
+  value = var.env
+
+}
